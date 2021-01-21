4adbb930060910decb6d3d71122624a19f585974
root <root@kut69222.info.kochi-tech.ac.jp>
2021-01-21 21:41:25 +0900

    fix 'processes' is not defind

--- client/client_scripts.py
+++ client/client_scripts.py
@@ -7,6 +7,8 @@ import os
 import time
 import socket
 import asyncio
+import openpyxl
+from datetime import datetime
 
 from get_info import getCECInfo
 from converter import i2b
@@ -44,6 +46,7 @@ def do_task(url, context, img, data_size):
   next_url = ''
   for calc in ['edge', 'cloud']:
     if task+1 in res['place'][calc]:
+      print('%s: %s'%(calc, res['calc_addr'][calc]))
       next_url = res['calc_addr'][calc]
 
   context = {
@@ -85,7 +88,7 @@ def do_task(url, context, img, data_size):
       result['task'+str(task)+'_calc'] = calc
       result['task'+str(task)] = res['times'][str(task)]
   if (len(place['edge']) != 0) and (len(place['cloud']) != 0):
-    print('c-e-c')
+    #print('c-e-c')
     result['calc'] = 'c-e-c'
     result['e-c'] = res['times']['edge'+str(place['edge'][0])]
     for calc in ['edge', 'cloud']:
@@ -95,21 +98,21 @@ def do_task(url, context, img, data_size):
     result['c-e'] = data_size*(1 if 1 in place['edge'] else ratio[0])/((run_time - (res['times']['1']+res['times']['2']+res['times']['3']) - 2*result['e-c'])/2)
     result['c-c'] = result['c-e']+result['e-c']
   elif (len(place['edge']) != 0) and (len(place['cloud']) == 0):
-    print('c-e')
+    #print('c-e')
     result['calc'] = 'c-e'
     result['c-e'] = data_size*(1 if 1 in place['edge'] else (ratio[0] if 2 in place['edge'] else ratio[1]))/((run_time - (res['times']['1']+res['times']['2']+res['times']['3']))/2)
   elif (len(place['cloud']) != 0) and (len(place['edge']) == 0):
-    print('c-c')
+    #print('c-c')
     result['calc'] = 'c-c'
     result['c-c'] = data_size*(1 if 1 in place['cloud'] else (ratio[0] if 2 in place['cloud'] else ratio[1]))/((run_time - (res['times']['1']+res['times']['2']+res['times']['3']))/2)
 
   ar_url = 'http://%s/controller/add_result'%calc_addr['edge']
   requests.post(ar_url, data=result)
-  #return result
+  return result
 
 def do_exis(file_name, url):
   img, data_size = open_file(file_name)
-  res_j = requests.get(url)
+  res_j = requests.get('http://%s/controller/repro'%url)
   calc_addr = json.loads(res_j.text)
   context = getCECInfo(calc_addr)
   context['data_size'] = data_size
@@ -137,6 +140,7 @@ def multi_do_exis(file_name, url, n):
 
   loop = asyncio.get_event_loop()
   results = loop.run_until_complete(do_async(loop))
+  return results
 
 def multi_do_prev(file_name, url, n):
   async def do_async(loop):
@@ -151,13 +155,64 @@ def multi_do_prev(file_name, url, n):
 
   loop = asyncio.get_event_loop()
   results = loop.run_until_complete(do_async(loop))
+  return results
 
-def multi_process_access(file_name, url, multi_n, process_n):
-  for i in range(process_n):
-    processes.apend(Process(target=multi_do_prev, args=(file_name, url, multi_n)))
+def multi_process_access(file_name, url, mn, n):
+  processes = []
+  for i in range(n):
+    processes.append(Process(target=multi_do_prev, args=(file_name, url, mn)))
     processes[i].start()
     time.sleep(1)
 
-  for i in range(process_n):
+  for i in range(n):
     processes[i].join()
     
+
+def client_test(file_name, url, n):
+  results = []
+  for i in range(n):
+    result = multi_do_prev(file_name, url, 1)
+    results.append(result)
+    time.sleep(1)
+
+  book = openpyxl.Workbook()
+  row_num = 1
+  row = sheet['a1':'o1']
+  row[0] = 'client_id'
+  row[1] = 'alg'
+  row[2] = 'ip_addr'
+  row[3] = 'pre'
+  row[4] = 'result'
+  row[5] = 'input_name'
+  row[6] = 'input_size'
+  row[7] = 'ans'
+  row[8] = 'confidence'
+  row[9] = 'pre_task1'
+  row[10] = 'pre_task2'
+  row[11] = 'pre_task3'
+  row[12] = 'res_task1'
+  row[13] = 'res_task2'
+  row[14] = 'res_task3'
+
+  for result in results:
+    row_num += 1
+    row = sheet[('a'+str(row_num)):('o'+str(row_num))] # 15個
+    row[0] = result['client_id']
+    row[1] = result['alg']
+    row[2] = result['ip_addr']
+    row[3] = result['pre']
+    row[4] = result['result']
+    row[5] = result['input_name']
+    row[6] = result['input_size']
+    row[7] = result['ans']
+    row[8] = result['confidence']
+    row[9] = result['pre_task1']
+    row[10] = result['pre_task2']
+    row[11] = result['pre_task3']
+    row[12] = result['res_task1']
+    row[13] = result['res_task2']
+    row[14] = result['res_task3']
+
+  date = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+  book.save('test_'+date+'.xlsx')
+
