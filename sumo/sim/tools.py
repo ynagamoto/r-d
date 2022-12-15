12c213e7ef90840f8b9cd6152bd3f2a2c87b54cd
munvei <roots19981108@gmail.com>
2022-12-15 10:48:11 +0900

    add print

--- sumo/sim/tools.py
+++ sumo/sim/tools.py
@@ -111,7 +111,7 @@ def setServersComm(sim_time:int, servers: List[Server], vehicles: Dict[str, Vehi
     for comm in vehicle.comm_list:
       beg, end = int(comm.time[0]), int(comm.time[1])
       print(f"beg: {beg}, end: {end}")
-      for i in range(beg, end+2): # 切り上げ
+      for i in range(beg, end+1): # 切り上げ
         servers_comm[i].append(vehicle.vid)
   return servers_comm
 
